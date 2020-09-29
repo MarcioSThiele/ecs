25b6ed3befe4ec4d2bef32c4acc117bcf77e7067
MarcioSThiele <marcio.sthiele@gmail.com>
2020-09-29 13:35:12 -0300

    some tests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.AmazonECSClientBuilder;
-import com.amazonaws.services.ecs.model.DescribeContainerInstancesRequest;
-import com.amazonaws.services.ecs.model.DescribeContainerInstancesResult;
+import com.amazonaws.services.ecs.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -31,16 +36,45 @@ public class DemoApplication {
 		//List<Task> listTask = describeTasksResult.getTasks();
 		//LOGGER.info("Teste: -> " + listTask.toString());
 
+		ListTasksRequest listTasksRequest = new ListTasksRequest();
+		listTasksRequest.setCluster("ecs-cluster-tef");
+		listTasksRequest.setServiceName("service-depositostef-acllimiteoperacional");
+
+		ListTasksResult listTasksResult = client.listTasks(listTasksRequest);
+		List<String> listString = listTasksResult.getTaskArns();
+
+		DescribeTasksRequest describeTasksRequest = new DescribeTasksRequest();
+		describeTasksRequest.setCluster("ecs-cluster-tef");
+		describeTasksRequest.setTasks(listString);
+
+		DescribeTasksResult describeTasksResult = client.describeTasks(describeTasksRequest);
+
+		List<Task> listTask = describeTasksResult.getTasks();
+		List<Container> listContainer = listTask.get(0).getContainers();
+
+		String containerArn = listContainer.get(0).getContainerArn();
+		int port = listContainer.get(0).getNetworkBindings().get(0).getHostPort();
+
 		DescribeContainerInstancesRequest describeContainerInstancesRequest = new DescribeContainerInstancesRequest();
 		describeContainerInstancesRequest.setCluster("ecs-cluster-tef");
-		describeContainerInstancesRequest.setContainerInstances(Arrays.asList("arn:aws:ecs:sa-east-1:875256826423:container-instance/ecs-cluster-tef/41c15f1b3311469ca70290ca718ccbae"));
+		describeContainerInstancesRequest.setContainerInstances(Arrays.asList(containerArn));
 
 		DescribeContainerInstancesResult describeContainerInstancesResult = client.describeContainerInstances(describeContainerInstancesRequest);
 
+		String listContainerInstance = describeContainerInstancesResult.getContainerInstances().get(0).getEc2InstanceId();
 
-		LOGGER.info("Teste -> " + describeContainerInstancesResult.toString());
+		AmazonEC2 amazonEC2 = AmazonEC2ClientBuilder.standard().build();
 
+		DescribeInstancesRequest describeInstancesRequestEc2 = new DescribeInstancesRequest();
+		describeInstancesRequestEc2.setInstanceIds(Arrays.asList(listContainerInstance));
 
+		DescribeInstancesResult describeInstancesResult = amazonEC2.describeInstances();
+
+		List<Reservation> reservations = describeInstancesResult.getReservations();
+		String ip = reservations.get(0).getInstances().get(0).getPrivateIpAddress();
+
+		LOGGER.info("ip -> " + ip);
+		LOGGER.info("port -> " + port);
 
 	}
 }
