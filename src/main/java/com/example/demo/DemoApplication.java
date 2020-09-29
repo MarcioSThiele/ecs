01e7b2f2ada3582f40ef7b4bebce810838aa3c07
MarcioSThiele <marcio.sthiele@gmail.com>
2020-09-29 12:15:46 -0300

    some tests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,14 @@ package com.example.demo;
 
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.AmazonECSClientBuilder;
-import com.amazonaws.services.ecs.model.DescribeTasksRequest;
-import com.amazonaws.services.ecs.model.DescribeTasksResult;
-import com.amazonaws.services.ecs.model.Task;
+import com.amazonaws.services.ecs.model.DescribeContainerInstancesRequest;
+import com.amazonaws.services.ecs.model.DescribeContainerInstancesResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -26,16 +24,23 @@ public class DemoApplication {
 	public static void access(){
 		AmazonECS client = AmazonECSClientBuilder.standard().build();
 
-		DescribeTasksRequest describeTasksRequest = new DescribeTasksRequest();
-		describeTasksRequest.setCluster("ecs-cluster-tef");
-		describeTasksRequest.setTasks(Arrays.asList("352c52c8ce1d48ebbebae3da4f6829b5"));
+		//DescribeTasksRequest describeTasksRequest = new DescribeTasksRequest();
+		//describeTasksRequest.setCluster("ecs-cluster-tef");
+		//describeTasksRequest.setTasks(Arrays.asList(""));
+		//DescribeTasksResult describeTasksResult = client.describeTasks(describeTasksRequest);
+		//List<Task> listTask = describeTasksResult.getTasks();
+		//LOGGER.info("Teste: -> " + listTask.toString());
+
+		DescribeContainerInstancesRequest describeContainerInstancesRequest = new DescribeContainerInstancesRequest();
+		describeContainerInstancesRequest.setCluster("ecs-cluster-tef");
+		describeContainerInstancesRequest.setContainerInstances(Arrays.asList("352c52c8ce1d48ebbebae3da4f6829b5"));
+
+		DescribeContainerInstancesResult describeContainerInstancesResult = client.describeContainerInstances(describeContainerInstancesRequest);
 
 
-		DescribeTasksResult describeTasksResult = client.describeTasks(describeTasksRequest);
+		LOGGER.info("Teste -> " + describeContainerInstancesResult.toString());
 
-		List<Task> listTask = describeTasksResult.getTasks();
 
-		LOGGER.info("Teste: -> " + listTask.toString());
 
 	}
 }
